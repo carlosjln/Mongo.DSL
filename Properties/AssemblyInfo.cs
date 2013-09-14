195299ad9ad17912ede169ae0cba987038766996
Carlos J. López <clopez@othersoft-inc.com>
2013-09-13 23:22:18 -0400

    Updated Assembly Informational Version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion( "1.0.0.*" )]
 //[assembly: AssemblyFileVersion( "1.0.0" )]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
